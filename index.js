0cc4c98d1a48ba7b92c91241bc0966f0807fb2e6
Connor <chopcraft@uchicago.edu>
2018-11-09 15:41:28 -0600

    error updates

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/pdf_gen/:id', function(req, res, next) {
 	var id = req.params.id;
 	var url = 'https://scratch.mit.edu/studios/' + id + '/';
 	//res.send(url);
-	/*
+	
 	var name = 'up' + Date.now() + '/'
   	mkdirp(name, function(err) {});
 	const child = execFile('./run_unit2gen.sh', [url, name, 'template/'], (error, stdout, stderr) => {
@@ -42,12 +42,14 @@ app.get('/pdf_gen/:id', function(req, res, next) {
 	  	rimraf(name, function () { console.log('done'); });
 	    //next(error);
 	  }
-	  console.log(stdout);
-	  res.sendFile(path.join(__dirname, name, 'all_tests.pdf'));
-	  rimraf(name, function () { console.log('done'); });
+	  else {
+	  	console.log(stdout);
+	  	res.sendFile(path.join(__dirname, name, 'all_tests.pdf'));
+	  	rimraf(name, function () { console.log('done'); });
+	  }
+	  
  	});
- 	*/
- 	res.send("test");
+ 	
 	
 
 	
