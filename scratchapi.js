8a1cc3aa1f152ff1757c9d17a049df83ec87b61a
mnwhite99 <mnwhite@uchicago.edu>
2019-06-05 15:30:10 -0500

    Update scratchapi.js

--- scratchapi.js
+++ scratchapi.js
@@ -100,7 +100,7 @@ Scratch.getProject = function(projectId, cb) {
   }, function(err, body, response) {
     if (err) return cb(err);
     try {
-      cb(null, body);
+      cb(null, JSON.parse(body));
     } catch (e) {
       //cb(e);
     }
